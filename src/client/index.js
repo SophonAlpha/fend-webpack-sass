36bb3607725f141931c426407a861eb8054a59b0
SophonAlpha <SophonAlpha@users.noreply.github.com>
2021-08-06 22:10:30 +0400

    webpack fixes

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,11 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
+export {
+    checkForName,
+    handleSubmit,
+}
+
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
